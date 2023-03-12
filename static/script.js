417ecaff49c91c5b9abc92bbb69da5126a367323
kangjian99 <80436148+kangjian99@users.noreply.github.com>
2023-03-12 14:45:56 +0800

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,26 @@
+let loading = document.getElementById('loading');
+let form = document.querySelector('form');
+form.addEventListener('submit', () => {
+    loading.style.display = 'block';
+});
+
+function copyToClipboard() {
+  var resText = document.getElementById("res").innerText;
+  navigator.clipboard.writeText(resText).then(function() {
+    var animation = document.getElementById("copy-animation");
+    animation.classList.remove("hide");
+    setTimeout(function() {
+      animation.classList.add("hide");
+    }, 2000);  // 2 秒后隐藏提示框
+  });
+}
+
+var dropdown = document.getElementById("dropdown");
+var pid = dropdown.getAttribute("data-pid");
+var pid_list = pid.split(",");
+for (var i = 1; i <= pid_list.length; i++) {
+  var option = document.createElement("option");
+  option.text = pid_list[i-1];
+  option.value = i;
+  dropdown.add(option);
+}
