3d2afeec6e3067041c92b5faf134cb4ac59bdcbe
kangjian99 <80436148+kangjian99@users.noreply.github.com>
2023-03-14 17:18:21 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, render_template, session, redirect, url_for, flash, jsonify
 import openai
 import os
+import re
+import csv
+from datetime import datetime
+import markdown2
 
 model = 'gpt-3.5-turbo' # or text-davinci-003
 # 从环境变量中读取API密钥
@@ -24,6 +28,7 @@ def send_gpt(prompt, tem):
         presence_penalty=0
         )
         print(f"{response['usage']}\n")
+        session['tokens'] = response['usage']['total_tokens']
 #        return response["choices"][0]["text"] # text-davinci-003
         message = response["choices"][0]['message']['content']
         messages.append({"role": "assistant", "content": message})
@@ -51,7 +56,7 @@ def get_request_json():
         if request.method == 'POST':
             print(request.form)
             if 'clear' in request.form:
-                session['messages'] == [] #不改变session['logged_in']
+                session['messages'] = [] #不改变session['logged_in']
                 return redirect(url_for('get_request_json'))
             else:
                 if len(request.form['question']) < 1:
@@ -75,13 +80,59 @@ def get_request_json():
                 print("Receive the keyword:", keyword)
                 print("Receive the temperature:", temperature)
                 res = send_gpt(question, temperature)
+                count_chars(res)
+                markdown_message = generate_markdown_message(res)
                 print("Q：\n", question)
                 print("A：\n", res)
-                return render_template('chat.html', model=model, question=keyword, res=str(res), temperature=temperature, pid = ",".join(prompts.keys()))
+                return render_template('chat.html', model=model, question=keyword, res=markdown_message, temperature=temperature, pid = ",".join(prompts.keys()))
         else:
-            session.clear()
+            session['messages'] = []
             return render_template('chat.html', model=model, question=0, pid = ",".join(prompts.keys()))
+
+def generate_markdown_message(text):
+    is_markdown = bool("#" in text or "*" in text or "|" in text) # 先判断是否markdown
+    if is_markdown:
+        text = text.replace("\n\n\n", "\n\n")
+        markdown_message = markdown2.markdown(text.strip(), extras=["tables"]) # 将返回的字符串转换为Markdown格式的HTML标记
+        return markdown_message
+    else:
+        return text
+
+def count_chars(text):
+    cn_pattern = re.compile(r'[\u4e00-\u9fa5\u3000-\u303f\uff00-\uffef]') #匹配中文字符及标点符号
+    cn_chars = cn_pattern.findall(text)
+
+    en_pattern = re.compile(r'[a-zA-Z]') #匹配英文字符
+    en_chars = en_pattern.findall(text)
+
+    cn_char_count = len(cn_chars)
+    en_char_count = len(en_chars)
+
+    now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    # 获取 session 中的统计结果列表，如果不存在则初始化为空列表
+    # results = session.get('results', [])
+    # 将当前统计结果添加为字典
+    stats = {'user_id': session.get('user_id'), 'datetime': now, 'cn_char_count': cn_char_count, 'en_char_count': en_char_count, 'tokens': session.get('tokens')}
+    print(stats)
     
+    # 将结果写入到 CSV 文件中
+    if stats:
+        write_results_to_csv(stats, 'stats.csv')
+        print(session)
+    
+    # 更新 session 中的统计结果列表
+    #session['results'] = results
+
+    return 'success'
+
+def write_results_to_csv(result, filename):
+    with open(filename, 'a', newline='') as file:  # 追加模式
+        writer = csv.DictWriter(file, fieldnames=result.keys())
+        if file.tell() == 0:
+            writer.writeheader()
+        writer.writerow(result)
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -91,6 +142,7 @@ def login():
             usernames = [line.strip() for line in f.readlines()]
         if username.lower() in [u.lower() for u in usernames] and (password == os.environ.get('Guest_PASSWORD') or password == os.environ.get('PASSWORD')):
             session['logged_in'] = True
+            session['user_id'] = username
             return redirect(url_for('get_request_json'))
         else:
             flash('用户名或密码错误')
@@ -100,7 +152,8 @@ def login():
 
 @app.route('/logout')
 def logout():
-    session.pop('logged_in', None)
+    # session.pop('logged_in', None)
+    session.clear()
     return redirect(url_for('get_request_json'))
 
 @app.route('/update-session', methods=['POST'])
