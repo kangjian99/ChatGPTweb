3073073ccdb67dd5de3f76cc7ef6972d724e3473
kangjian99 <80436148+kangjian99@users.noreply.github.com>
2023-03-14 17:19:27 +0800

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -5,15 +5,26 @@ form.addEventListener('submit', () => {
 });
 
 function copyToClipboard() {
-  var resText = document.getElementById("res").innerText;
-  navigator.clipboard.writeText(resText).then(function() {
-    var animation = document.getElementById("copy-animation");
-    animation.classList.remove("hide");
-    setTimeout(function() {
-      animation.classList.add("hide");
-    }, 2000);  // 2 秒后隐藏提示框
-  });
+  var resHtml = document.getElementById("res").innerHTML;
+  var tmpElement = document.createElement('div');
+  tmpElement.innerHTML = resHtml;
+  document.body.appendChild(tmpElement);
+  tmpElement.style.position = 'absolute';
+  tmpElement.style.left = '-9999px';
+  var range = document.createRange();
+  range.selectNodeContents(tmpElement);
+  var selection = window.getSelection();
+  selection.removeAllRanges();
+  selection.addRange(range);
+  document.execCommand('copy', false, 'text/html');
+  document.body.removeChild(tmpElement);
+  var animation = document.getElementById("copy-animation");
+  animation.classList.remove("hide");
+  setTimeout(function() {
+    animation.classList.add("hide");
+  }, 2000);  // 2 秒后隐藏提示框
 }
+
 
 var dropdown = document.getElementById("dropdown");
 var pid = dropdown.getAttribute("data-pid");
