2edb582765d7a718bfefdac69b0ae42ddc5f90b3
kangjian99 <80436148+kangjian99@users.noreply.github.com>
2023-03-19 09:40:45 +0800

    Add files via upload

--- md_process.py
+++ md_process.py
@@ -6,9 +6,15 @@ def generate_markdown_message(text):
         text = text[2:]
     pattern = r'#{2,6}(?!\w)|\*\*[\s\S]*?\*\*|\*[\s\S]*?\*|\||^-{1,}\s|(?<!\S)```(?!\S)'
     is_markdown = re.search(pattern, text) # 先判断是否markdown
+    is_codeblock = bool('```' in text)
     if is_markdown:
         text = text.replace("\n\n\n", "\n\n")
+        if is_codeblock:
+            text = text.replace("#", "%35%")
         markdown_message = markdown.markdown(text, extensions=["tables", "nl2br"]) # 将返回的字符串转换为Markdown格式的HTML标记
+        if is_codeblock:
+            markdown_message = markdown_message.replace("%35%", "#")
         return markdown_message
     else:
+        text = text.replace("\n", "<br>")
         return text
