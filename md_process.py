84472de4e27041ee96eae95282d0509e46454f59
kangjian99 <80436148+kangjian99@users.noreply.github.com>
2023-03-27 11:50:33 +0800

    Add files via upload

--- md_process.py
+++ md_process.py
@@ -1,9 +1,20 @@
 import re
 import markdown
+import html
+
+def is_html(text):
+    html_pattern = re.compile(r'<[^>]+>')
+    return bool(html_pattern.search(text))
 
 def generate_markdown_message(text):
     if text.startswith("\n\n"):
         text = text[2:]
+        
+    if is_html(text):
+        escaped_text = html.escape(text) # 将HTML标签转换为实体
+        text = f"<pre>{escaped_text}</pre>"
+        return text
+
     pattern = r'#{2,6}(?!\w)|\*\*[\s\S]*?\*\*|\*[\s\S]*?\*|\||^-{1,}\s|```'
     is_markdown = re.search(pattern, text) # 先判断是否markdown
     is_codeblock = bool('```' in text)
