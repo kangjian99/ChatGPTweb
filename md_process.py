0360efded801098b53bdb7bf187af7e8f10bda99
kangjian99 <80436148+kangjian99@users.noreply.github.com>
2023-03-27 16:38:46 +0800

    Update md_process.py

--- md_process.py
+++ md_process.py
@@ -17,15 +17,21 @@ def generate_markdown_message(text):
 
     pattern = r'#{2,6}(?!\w)|\*\*[\s\S]*?\*\*|\*[\s\S]*?\*|\||^-{1,}\s|```'
     is_markdown = re.search(pattern, text) # 先判断是否markdown
-    is_codeblock = bool('```' in text)
     if is_markdown:
         text = text.replace("\n\n\n", "\n\n")
+        is_codeblock = bool('```' in text)
         if is_codeblock:
             text = text.replace("#", "%35%")
             text = text.replace("\n\n", "\n")
+            # 获取text中```出现的次数
+            num_ticks = text.count('```')
+            # 如果出现次数是奇数，则在text的末尾添加```
+            if num_ticks % 2 == 1:
+                text += '```'
         markdown_message = markdown.markdown(text, extensions=["tables", "nl2br"]) # 将返回的字符串转换为Markdown格式的HTML标记
         if is_codeblock:
             markdown_message = markdown_message.replace("%35%", "#")
+            markdown_message = re.sub(r'^#', '\n#', markdown_message, flags=re.MULTILINE)
         return markdown_message
     else:
         text = text.replace("\n", "<br>")
